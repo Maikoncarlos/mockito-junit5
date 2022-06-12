30a2eeff87ccc82d5755e7481d4c72a0e744e1c1
Maikoncarlos <maikonsaturno@gmail.com>
2022-06-12 12:48:38 -0300

    modificando usercontroller para não devolver estatico

--- src/main/java/com/github/maikoncarlos/api/rest/testes/mockito/junit5/resources/UserController.java
+++ src/main/java/com/github/maikoncarlos/api/rest/testes/mockito/junit5/resources/UserController.java
@@ -1,6 +1,8 @@
 package com.github.maikoncarlos.api.rest.testes.mockito.junit5.resources;
 
 import com.github.maikoncarlos.api.rest.testes.mockito.junit5.domain.User;
+import com.github.maikoncarlos.api.rest.testes.mockito.junit5.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Maikon", "maikon@maikon.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
