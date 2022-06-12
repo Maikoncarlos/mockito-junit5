5955af75acb2bf48d83b4591118b56e16bd359bb
Maikoncarlos <maikonsaturno@gmail.com>
2022-06-12 12:49:53 -0300

    inserindo classe serviceImpl findbyid

---
+++ src/main/java/com/github/maikoncarlos/api/rest/testes/mockito/junit5/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.github.maikoncarlos.api.rest.testes.mockito.junit5.services.impl;
+
+import com.github.maikoncarlos.api.rest.testes.mockito.junit5.domain.User;
+import com.github.maikoncarlos.api.rest.testes.mockito.junit5.repositories.UserRepository;
+import com.github.maikoncarlos.api.rest.testes.mockito.junit5.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> userOpt = repository.findById(id);
+        return userOpt.orElse(null);
+    }
+}
