9acf70f33c2766c729a69115660c9b87c1df853e
Maikoncarlos <maikonsaturno@gmail.com>
2022-06-11 21:49:28 -0300

    commit inicial do projeto

---
+++ src/main/java/com/github/maikoncarlos/api/rest/testes/mockito/junit5/Application.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.api.rest.testes.mockito.junit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
