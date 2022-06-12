f8cb1ac94799e10e9bb2499f759b798436de26a8
Maikoncarlos <maikonsaturno@gmail.com>
2022-06-12 13:27:02 -0300

    tratamento de excecao para objeto User nao encontrado

---
+++ src/main/java/com/github/maikoncarlos/api/rest/testes/mockito/junit5/resources/exceptions/StandardError.java
@@ -0,0 +1,18 @@
+package com.github.maikoncarlos.api.rest.testes.mockito.junit5.resources.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class StandardError {
+
+    private LocalDateTime timestamp;
+    private String status;
+    private String error;
+    private String path;
+}
--- src/main/java/com/github/maikoncarlos/api/rest/testes/mockito/junit5/services/impl/UserServiceImpl.java
+++ src/main/java/com/github/maikoncarlos/api/rest/testes/mockito/junit5/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.github.maikoncarlos.api.rest.testes.mockito.junit5.services.impl;
 import com.github.maikoncarlos.api.rest.testes.mockito.junit5.domain.User;
 import com.github.maikoncarlos.api.rest.testes.mockito.junit5.repositories.UserRepository;
 import com.github.maikoncarlos.api.rest.testes.mockito.junit5.services.UserService;
+import com.github.maikoncarlos.api.rest.testes.mockito.junit5.services.exceptions.UserNotfoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> userOpt = repository.findById(id);
-        return userOpt.orElse(null);
+        return userOpt.orElseThrow(()-> new UserNotfoundException("Objeto User não encontrado"));
     }
 }
